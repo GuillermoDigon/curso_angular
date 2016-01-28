f73951103a4158a03c228b10a73c9a9206aecc04
Juan Jesús Gómez Noya <burus_86@hotmail.com>
2016-01-28 18:07:32 +0100

    Actualizado el repositorio con los ejercicios realizados hasta el día 27/01/2016

---
+++ hola_yeoman/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name holaYeomanApp
+ * @description
+ * # holaYeomanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('holaYeomanApp', []);
---
+++ hola_yeoman/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holaYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the holaYeomanApp
+ */
+angular.module('holaYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
